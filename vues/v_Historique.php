982ef9f229d4d64698167371a1c40a75572d34a4
cedricmart8 <cedricmart8@gmail.com>
2017-04-24 16:02:10 +0200

    maj historique

--- vues/v_Historique.php
+++ vues/v_Historique.php
@@ -2,16 +2,15 @@
     <h2>Historique</h2>
     <?php
     foreach ($lesEntretiens as $unEntretien){
-      echo '<from action="index.php?uc=gererEntretien&action=recapEntretien" method="POST">';
-      echo 'Entretien '. $unEntretien['id'];
+      echo '<form method="POST" action="index.php?uc=gererEntretien&action=recapEntretien">';
+      echo 'Entretien '.$unEntretien['id'];
       echo '<br>';
       echo 'Date : '.$unEntretien['jour'];
       echo '<br>';
       echo '<input type="submit" value="Details" name="Details">';
       echo '<br>';
-      echo "-------------";
       echo '<br>';
-      echo '</from>';
+      echo '</form>';
     }
      ?>
 </div>
