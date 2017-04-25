6e7f6f7786da2d0df8b77cc8e8cc9717c5d97975
cedricmart8 <cedricmart8@gmail.com>
2017-04-25 10:50:13 +0200

    changement style sommaire

--- vues/v_sommaire.php
+++ vues/v_sommaire.php
@@ -10,28 +10,28 @@
         <ul id="menuList">
         <li>
           Visiteur :<br>
-          <?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
+          <?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?><br><br>
         </li>
           <li class="smenu">
-              <a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a>
+              <a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a><br>
            </li>
            <li class="smenu">
-              <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
+              <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a><br>
            </li>
            <?php
            $idVisiteur=$_SESSION['idVisiteur'];
            $grade=$pdo->getGrade($idVisiteur);
            if($grade[0]['codeGrade']!=1 && $grade[0]['codeGrade']!=null){?>
              <li class="smenu">
-              <a href="index.php?uc=gererEntretien&action=EnregistrerEntretien" title="Ajout des entretiens">Entretien</a>
+              <a href="index.php?uc=gererEntretien&action=EnregistrerEntretien" title="Ajout des entretiens">Entretien</a><br>
            </li><?php
            }?>
            <li class="smenu">
-              <a href="index.php?uc=gererHistorique&action=voirHistorique" title="Historique des entretiens">Historique entretien</a>
+              <a href="index.php?uc=gererHistorique&action=voirHistorique" title="Historique des entretiens">Historique entretien</a><br>
            </li>
          </br>
  	         <li class="smenu">
-              <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
+              <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a><br>
            </li>
          </ul>
 
